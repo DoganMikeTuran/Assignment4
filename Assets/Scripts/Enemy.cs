3f78b87b1154374295f39c23e2f51e4b6229c37f
Dogan Mike Turan <doganmiketuran@live.dk>
2019-09-22 23:16:31 +0200

    dsss

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public float speed = 10f;
+    private Transform target;
+    private int wavePointIndex = 0;
+
+// Start is called before the first frame update
+void Start()
+    {
+
+        target = WayPoints.points[0]; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f) {
+            GetNextWayPoint();
+        }
+    }
+    void GetNextWayPoint() {
+        if (wavePointIndex >= WayPoints.points.Length - 1) {
+            Destroy(gameObject);
+            return;
+                }
+        wavePointIndex++;
+        target = WayPoints.points[wavePointIndex];
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject spawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+    public Text waveCountDownText;
+    private float waveNumber = 0f;
+    void Update()
+    {
+        if (countDown <= 0f) {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+
+        }
+        countDown -= Time.deltaTime;
+
+        waveCountDownText.text = Mathf.Round(countDown).ToString();
+        
+    }
+
+    IEnumerator SpawnWave() {
+        waveNumber++;
+        for (int i = 0; i < waveNumber ; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.56f);
+        }
+
+        
+    }
+
+    void SpawnEnemy() {
+        Instantiate(enemyPrefab,spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+    }
+}
